b069ffae35875c409d1d80954af4bce71ff036e0
Gabe Shaughnessy <gabeshaughnessy@gmail.com>
2012-01-07 18:20:38 -0800

    Removed index.html and set root to jobs index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Learningrails::Application.routes.draw do
   resources :jobs
 
+	root:to => 'jobs#index'
+	
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
